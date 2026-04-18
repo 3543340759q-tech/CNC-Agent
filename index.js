c3b256807863ee6d17a1c10e1f647f04171d1ee1
3543340759q-tech <3543340759q@gmail.com>
2026-04-19 05:00:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 app.get('/', (req, res) => res.send('<h1>17岁生日快乐！CNC Agent 部署成功。</h1>'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
